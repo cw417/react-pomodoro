1da2959138b544843bba7501e599a312658168c1
cw417 <chriswhitlock417@gmail.com>
2022-11-14 15:48:04 -0500

    Implement count down timer; format time display

--- client/src/components/PomodoroTimer/PomodoroTimer.tsx
+++ client/src/components/PomodoroTimer/PomodoroTimer.tsx
@@ -6,17 +6,30 @@ type Props = {
 
 export default function PomodoroTimer({ stylingClasses }: Props) {
   
-  const [time, setTime]: [number, Dispatch<SetStateAction<number>>] = useState(0);
+  const [time, setTime]: [number, Dispatch<SetStateAction<number>>] = useState(1500);
+  const [displayTime, setDisplayTime]: [string, Dispatch<SetStateAction<string>>] = useState("25:00")
   
   // timer
   useEffect(() => { 
     const timer = setInterval(() => {
-      setTime((time) => time + 1);
+      setTime((time) => time - 1);
     }, 1000);
     return () => clearInterval(timer);
    }, []);
 
+  function formatTime(secondsToConvert: number) {
+    /**
+     * Converts seconds to a time string in the format of mm:ss.
+     * @param {number} seconds    Number of seconds to format
+     */
+    const minutes: number = Math.floor(secondsToConvert / 60);
+    const minutesString: string = (minutes < 10) ? `0${minutes}` : minutes.toString();
+    const seconds: number = secondsToConvert % 60;
+    const secondsString: string = (seconds < 10) ? `0${seconds}` : seconds.toString();
+    return `${minutesString}:${secondsString}`
+  }
+
   return (
-    <div className={`${stylingClasses}`}>Time: {time}</div>
+    <div className={`${stylingClasses}`}>Time: {formatTime(time)}</div>
   )
 }
